4a0e21ce21aed19fabea604ac256e1deda18faff
sahrialardians <sahrialardians@gmail.com>
2020-12-10 20:05:08 +0700

    create relationship user and subject

--- app/Models/Article/Article.php
+++ app/Models/Article/Article.php
@@ -2,9 +2,23 @@
 
 namespace App\Models\Article;
 
+use App\Article\Subject;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'body', 'subject_id'];
+
+    // setiap article dipunyai satu user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // setiap article dipunyai satu subject
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
 }
