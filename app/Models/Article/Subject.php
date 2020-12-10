55313e308cef3bd9c9b8935bbc599c579f2a757b
sahrialardians <sahrialardians@gmail.com>
2020-12-10 20:05:25 +0700

    create relationship articles

--- app/Models/Article/Subject.php
+++ app/Models/Article/Subject.php
@@ -2,9 +2,14 @@
 
 namespace App\Models\Article;
 
+use App\Models\Article\Article;
 use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    //
+    // Setiap subject mempunyai banyak article
+    public function articles()
+    {
+        return $this->hasMany(Article::class );
+    }
 }
